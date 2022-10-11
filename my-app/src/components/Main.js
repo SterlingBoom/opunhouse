62bf3bf72ae0ce9673bbbb1d36e328191fbe25ac
SterlingBoom <bummiecarez@gmail.com>
2022-10-11 17:25:27 +0100

    Opun house coming soon page

---
+++ my-app/src/components/Main.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+      <div className='opunhouse-wrapper'>
+        <div className='web3-container'>
+          <div>
+            <p className='opun-house'>opunhouse.com</p>
+            <h2 className='web3'>A Web3 Community For Real Estate Investors</h2>
+            <a
+              target='_blank'
+              rel='noreferrer'
+              className ='wait'
+              href='https://docs.google.com/forms/d/16QZiqesiw0-CVDlmN9zlED8fbKMET5cr278Coohxd6U/edit?ts=633cc2f7'
+            >
+             <p className='waitlist'> Join the Waitlist</p>
+            </a>
+
+            <h6 className='coming-2023'>Coming 2023</h6>
+          </div>
+          <div>
+            <img src='/Group.png' alt='' className='img-size'></img>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+export default Main
